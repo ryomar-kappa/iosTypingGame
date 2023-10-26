02a29ecbc7597707e8a3518b4642776dd8714c20
ryomar-kappa <ryomaryoma777@yahoo.co.jp>
2023-10-27 00:54:28 +0900

    図鑑の詳細を仮実装

--- lib/screen/zukan/zukan_view.dart
+++ lib/screen/zukan/zukan_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:takayama_test/main.dart';
+import 'package:takayama_test/model/fish.dart';
 
 class ZukanView extends StatelessWidget {
   const ZukanView({super.key});
@@ -16,17 +17,39 @@ class ZukanView extends StatelessWidget {
               crossAxisSpacing: 5,
               childAspectRatio: 2.0,
             ),
-            itemBuilder: (_, index) => SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Column(children: [
-                    Text(fishList[index].kanji),
-                    Text(fishList[index].yomigana),
-                    SizedBox(
-                        height: 50, child: Image.asset(fishList[index].path))
-                  ]),
+            itemBuilder: (_, index) => GestureDetector(
+                  onTap: () => showDialog(
+                      context: context,
+                      builder: (context) =>
+                          OsakanaSyousai(fish: fishList[index])),
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Column(children: [
+                      Text(fishList[index].kanji),
+                      Text(fishList[index].yomigana),
+                      SizedBox(
+                          height: 50, child: Image.asset(fishList[index].path))
+                    ]),
+                  ),
                 )),
       ),
     );
+  }
+}
+
+class OsakanaSyousai extends StatelessWidget {
+  const OsakanaSyousai({super.key, required this.fish});
+
+  final Fish fish;
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(children: [
+      Image.asset(fish.path),
+      const Text(
+        'とても美味しい',
+        style: TextStyle(fontSize: 24),
+      )
+    ]);
   }
 }
